349fc40d3506cf91fabbab34e25e46c76f5a90a3
Kef1r1k <enikiforoff2@yandex.ru>
2024-02-06 21:26:36 +0300

    test commit

--- Refs-app/ContentView.swift
+++ Refs-app/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("testest Teeeeeest")
+            Text("testest Teeeeeest :3")
         }
         .padding()
     }
