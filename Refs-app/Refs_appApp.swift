9fda95a2834da3095ff29da8980b90cc9455c87d
Kef1r1k <enikiforoff2@yandex.ru>
2024-02-06 21:25:10 +0300

    init commit

---
+++ Refs-app/Refs_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Refs_appApp.swift
+//  Refs-app
+//
+//  Created by Екатерина Никифорова on 06.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct Refs_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
